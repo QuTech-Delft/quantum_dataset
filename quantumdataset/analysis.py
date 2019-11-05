e923779ac72bb26dbe275b5ac7e45f25ee405b8c
Pieter Eendebak <P.T.eendebak@tudelft.nl>
2019-11-05 14:49:23 +0100

    Feature/cleanup (#2)
    * add type annotations
    
    * convert example to jupiter notebook
    
    * add allxy

--- quantumdataset/analysis.py
+++ quantumdataset/analysis.py
@@ -1,5 +1,5 @@
 import numpy as np
-from qcodes import DataSet 
+from qcodes import DataSet
 
 import qtt
 import qtt.algorithms.functions
@@ -12,30 +12,35 @@ from qtt.data import default_setpoint_array
 from qtt.algorithms.random_telegraph_signal import tunnelrates_RTS, fit_double_gaussian, _plot_rts_histogram
 import qtt.algorithms.allxy
 
-def _parse_1d_dataset(dataset : DataSet) -> tuple:
+
+def _parse_1d_dataset(dataset: DataSet) -> tuple:
     y_data = np.array(dataset.default_parameter_array())
     x_data = np.array(qtt.data.default_setpoint_array(dataset))
     return x_data, y_data
 
-def analyse_coulomb(dataset : DataSet, fig : int) -> dict:
+
+def analyse_coulomb(dataset: DataSet, fig: int = 1) -> dict:
     """ Analyse dataset with Coulomb peaks """
     parameters = {'typicalhalfwidth': 2}
-    qtt.algorithms.coulomb.analyseCoulombPeaks(dataset, fig=fig, verbose=2, parameters=parameters)
+    qtt.algorithms.coulomb.analyseCoulombPeaks(dataset, fig=fig, verbose=0, parameters=parameters)
     plt.legend()
     return {}
 
-def analyse_pinchoff(dataset : DataSet, fig : int) -> dict:
+
+def analyse_pinchoff(dataset: DataSet, fig: int = 1) -> dict:
     result = qtt.algorithms.gatesweep.analyseGateSweep(dataset, fig=fig)
     plt.legend()
     return result
 
 
-def analyse_allxy(dataset : DataSet, fig : int) -> dict:
+def analyse_allxy(dataset: DataSet, fig: int = 1) -> dict:
     result = qtt.algorithms.allxy.fit_allxy(dataset)
     qtt.algorithms.allxy.plot_allxy(dataset, result, fig=fig)
+    plt.subplots_adjust(bottom=.15)
     return result
 
-def analyse_time_rabi(dataset : DataSet, fig : int) -> dict:
+
+def analyse_time_rabi(dataset: DataSet, fig: int = 1) -> dict:
     x_data, y_data = _parse_1d_dataset(dataset)
     fit_parameters, xx = qtt.algorithms.functions.fit_gauss_ramsey(x_data, y_data)
     qtt.algorithms.functions.plot_gauss_ramsey_fit(x_data, y_data, fit_parameters, fig=fig)
@@ -43,17 +48,17 @@ def analyse_time_rabi(dataset : DataSet, fig : int) -> dict:
     return {'fit_parameters': fit_parameters}
 
 
-def analyse_time_ramsey(dataset : DataSet, fig : int) -> dict:
+def analyse_time_ramsey(dataset: DataSet, fig: int = 1) -> dict:
     x_data, y_data = _parse_1d_dataset(dataset)
     fit_parameters, xx = qtt.algorithms.functions.fit_gauss_ramsey(x_data, y_data)
     qtt.algorithms.functions.plot_gauss_ramsey_fit(x_data, y_data, fit_parameters, fig=fig)
     return {'fit_parameters': fit_parameters}
 
 
-def analyse_polarization_line(dataset : DataSet, fig : int, verbose=1) -> dict:
+def analyse_polarization_line(dataset: DataSet, fig: int = 1, verbose=0) -> dict:
     """  Analyse dataset with polarization line """
     if verbose:
-        print('pol_fitting on dataset: %s' % dataset.location)
+        print('analyse_polarization_line: dataset: %s' % dataset.location)
     signal = dataset.default_parameter_array()
     delta = default_setpoint_array(dataset, signal.name)
 
@@ -69,7 +74,7 @@ def analyse_polarization_line(dataset : DataSet, fig : int, verbose=1) -> dict:
     return {}
 
 
-def analyse_RTS(dataset : DataSet, fig : int) -> dict:
+def analyse_RTS(dataset: DataSet, fig: int = 1) -> dict:
     time = default_setpoint_array(dataset)
     rtsdata = np.array(dataset.default_parameter_array())
     num_bins = 40
--- quantumdataset/tests/test_quantum_dataset.py
+++ quantumdataset/tests/test_quantum_dataset.py
@@ -3,11 +3,11 @@ import unittest
 
 from quantumdataset import QuantumDataset
 
+
 class TestQuantumDataset(unittest.TestCase):
 
-
     def test_check_quantum_dataset_installation(self):
-        
+
         with tempfile.TemporaryDirectory() as tmpdir:
             result = QuantumDataset.check_quantum_dataset_installation(tmpdir)
             self.assertIsNone(result)
