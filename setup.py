ea074beec722365b6d607a1d86a69d05a4392599
Pieter Eendebak <pieter.eendebak@tno.nl>
2020-09-04 13:57:57 +0200

    fix ci

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(name='quantum_dataset',
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Science/Research',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
           'Topic :: Scientific/Engineering'
