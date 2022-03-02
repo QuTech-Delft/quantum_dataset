34b973cd9a002f83bb7e7130ae6e72a98fd6dc84
Pieter Eendebak <pieter.eendebak@gmail.com>
2022-03-02 16:17:15 +0100

    move to v2

--- quantumdataset/tests/test_quantum_dataset.py
+++ quantumdataset/tests/test_quantum_dataset.py
@@ -1,3 +1,5 @@
+import contextlib
+import io
 import os
 import tempfile
 import unittest
@@ -7,9 +9,8 @@ from quantumdataset import QuantumDataset
 
 
 class TestQuantumDataset(unittest.TestCase):
-
     def setUp(self):
-        datadir = os.environ.get('QUANTUM_DATASET_TEST_DIR')
+        datadir = os.environ.get("QUANTUM_DATASET_TEST_DIR")
         if datadir is None:
             datadir = tempfile.mkdtemp()
         self.qd = QuantumDataset(datadir)
@@ -24,10 +25,24 @@ class TestQuantumDataset(unittest.TestCase):
         tags = self.qd.list_tags()
         self.assertIsInstance(tags, List)
 
+        subtags = self.qd.list_subtags("allxy")
+        self.assertIsInstance(subtags, list)
+
     def test_load_dataset(self):
-        ds = self.qd.load_dataset('frequency_rabi', 0)
+        ds = self.qd.load_dataset("time_rabi", 0)
         self.assertTrue(ds is not None)
 
+    def test_show_data(self):
+        with contextlib.redirect_stdout(io.StringIO()) as s:
+            self.qd.show_data()
+        self.assertIn("tag allxy", s.getvalue())
 
-if __name__ == '__main__':
+    def test_database_metadata(self):
+        m = self.qd.metadata()
+        self.assertIsInstance(m, list)
+
+        self.assertIsInstance(m[0].tag, str)
+
+
+if __name__ == "__main__":
     unittest.main()
